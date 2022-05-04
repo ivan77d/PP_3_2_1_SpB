3e08b5ad259d38b72a4d47420ca6cb496003130d
ivan77d <ivandrokin@gmail.com>
2022-05-04 11:03:10 +0300

    1

---
+++ src/main/java/com/example/pp_3_2_1_spb/Pp321SpBApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_2_1_spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp321SpBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp321SpBApplication.class, args);
+	}
+
+}
